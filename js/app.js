019521b38b0bc9bcd97f9045c80feb7a436bb84b
Gary Darnell <gdarnell9@gmail.com>
2021-06-18 11:18:58 +0100

    Added required fields and delete all button to html.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-const newPlayerAdd = document.querySelector('#new-player-form');
-newPlayerAdd.addEventListener('submit', handlePlayerAdd);
+
+    const newPlayerAdd = document.querySelector('#new-player-form');
+    newPlayerAdd.addEventListener('submit', handlePlayerAdd);
 
 })
 
 const handlePlayerAdd = function (event) {
     event.preventDefault();
-    console.log(event);
+    console.log(event)
+
 }
